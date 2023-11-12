d868fc74d70969b399ec6034f5fa68db7d1b2d0a
yok939 <james0920pig@gmail.com>
2023-11-12 22:21:47 +0800

    11/12/2023 22:21

--- TOI Expert/e810.cpp
+++ TOI Expert/e810.cpp
@@ -22,22 +22,7 @@ int main(){
         }
         cout << '\n';
     }
-    for(int k=0;k<n;k++){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                printf("%d %d %d\n", i, j, k);
-                if(graph[i][j]>graph[i][k]+graph[k][j]&&graph[k][j]!=INT_MAX&&graph[i][k]!=INT_MAX){
-                    graph[i][j]=graph[i][k]+graph[k][j];
-                }
-            }
-        }
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout << graph[i][j] << ' ';
-        }
-        cout << '\n';
-    }
+    
     
 }
 
