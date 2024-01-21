0e0a0dee9f303be08f2290a7633da121b0033041
yok939 <james0920pig@gmail.com>
2024-01-21 18:49:57 +0800

    fixed untracked files

--- TOI Beginner Gathering/f337.cpp
+++ TOI Beginner Gathering/f337.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 int main(){
-    cout << "ehel";
+    int n, m, i=0, j=0;
+    cin >> n >> m;
+    i=0, j=n;
+    do{
+        if(i*2+j*3==m*8){
+            cout << "Yes";
+            return 0;
+        }
+        i++;j--;
+    }while(i<=n);
+    if(i*2+j*3>m*8){
+        cout << "Not enough";
+    }
+    else if(i*2+j*3<m*8){
+        cout << "Too much";
+    }
     return 0;
 }
