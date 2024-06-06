52844eb262d471992ac6aba1101935fa80aff967
yok939 <james0920pig@gmail.com>
2024-06-06 19:18:16 +0800

    06/06/2024 19:18

--- APCS/APCS P2/f606.cpp
+++ APCS/APCS P2/f606.cpp
@@ -1,27 +1,33 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
+int n, m, k, q[55][55], c[55][55], ans = INT_MAX;
+
 int main(){
-    int n, m, k, q[55][55], c[55][55], location=0, addup=0, total=0, min_total=INT_MAX;
     cin >> n >> m >> k;
-    for(int i=0;i<n;i++) for(int j=0;j<m;j++) cin >> q[i][j];
-    for(int i=0;i<k;i++) for(int j=0;j<n;j++) cin >> c[i][j];
-    for(int i=0;i<k;i++){
-        total=0;
-        for(int j=0;j<m;j++){
-            location=j;
-            for(int a=0;a<m;a++){
-                addup=0;
-                for(int b=0;b<n;b++){
-                    if(c[i][b]==j){
-                        addup+=q[b][a];
-                    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> q[i][j];
+        }
+    }
+    for(int i = 0; i < k; i++){
+        for(int j = 0; j < n; j++){
+            cin >> c[i][j];
+        }
+    }
+    for(int i = 0; i < k; i++){
+        int total = 0;
+        for(int j = 0; j < m; j++){
+            for(int a = 0; a < m; a++){
+                int addup = 0;
+                for(int b = 0; b < n; b++){
+                    if(c[i][b] == j) addup += q[b][a];
                 }
-                total+=(location==a?addup:addup>1000?3000+(addup-1000)*2:addup*3);
+                total += (j == a ? addup : addup > 1000 ? 3000 + (addup - 1000) * 2 : addup * 3);
             }
         }
-        min_total=min(total, min_total);
+        ans = min(ans, total);
     }
-    cout << min_total;
+    cout << ans << '\n';
+    return 0;
 }
