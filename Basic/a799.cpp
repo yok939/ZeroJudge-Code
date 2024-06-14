a92f8843fc468968ec92f75eab26c673a2aac1b0
yok939 <james0920pig@gmail.com>
2024-06-14 09:39:43 +0800

    06/14/2024 09:39

---
+++ Basic/a799.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main(){
+    cin >> n;
+    cout << abs(n) << '\n';
+    return 0;
+}
--- TOI Beginner/i377.cpp
+++ TOI Beginner/i377.cpp
@@ -1,167 +1,42 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-bool ismatch(string target, char map[75][75], int x_start, int y_start, int x_add, int y_add){
-    int x=x_start, y=y_start;
-    for(int i=0;i<target.length();i++){
-        if(map[y][x]==target[i]){
-            x+=x_add, y+=y_add;
-        }
-        else{
-            return false;
-        }
-    }
-    return true;
+int r, c, dir[8][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+int ansx, ansy;
+char grid[80][80];
+string s;
+
+bool in_range(int x, int y, int tarx, int tary){
+    return x >= 1 && y >= 1 && x <= tarx && y <= tary; 
 }
-
-
 
 int main(){
-    int r, c, top_x=0, top_y=0, bottom_x=0, bottom_y=0;
-    int possible_top_x[5630], possible_top_y[5630], possible_bottom_x[5630], possible_bottom_y[5630], total=0, same=1, min=INT_MAX, min_i;
-    string s;
-    char map[75][75];
     cin >> r >> c;
-    for(int i=0;i<r;i++)for(int j=0;j<c;j++)cin >> map[i][j];
-    for(int i=0;i<r;i++)for(int j=0;j<c;j++)if(map[i][j]>=65&&map[i][j]<=90)map[i][j]=map[i][j]-'A'+'a';
+    for(int i = 1; i <= r; i++){
+        for(int j = 1; j <= c; j++){
+            cin >> grid[i][j];
+        }
+    }
     cin >> s;
-    for(int i=0;i<s.length();i++)if(s[i]>=65&&s[i]<=90)s[i]=s[i]-'A'+'a';
-    // left to right, right to left
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c-s.length()+1;j++){
-            top_x=j, top_y=i, bottom_x=s.length()-1+j, bottom_y=i;
-            if(ismatch(s, map, top_x, top_y, 1, 0)){
-                possible_top_x[total]=top_x, possible_top_y[total]=top_y, possible_bottom_x[total]=bottom_x, possible_bottom_y[total]=bottom_y;
-                total++;
-            }
-            if(ismatch(s, map, bottom_x, bottom_y, -1, 0)){
-                possible_top_x[total]=bottom_x, possible_top_y[total]=bottom_y, possible_bottom_x[total]=top_x, possible_bottom_y[total]=top_y;
-                total++;
-            }
-        }
-    }
-    // top to bottom, bottom to top
-    for(int i=0;i<r-s.length()+1;i++){
-        for(int j=0;j<c;j++){
-            top_x=j, top_y=i, bottom_x=j, bottom_y=s.length()-1+i;
-            if(ismatch(s, map, top_x, top_y, 0, 1)){
-                possible_top_x[total]=top_x, possible_top_y[total]=top_y, possible_bottom_x[total]=bottom_x, possible_bottom_y[total]=bottom_y;
-                total++;
-            }
-            if(ismatch(s, map, bottom_x, bottom_y, 0, -1)){
-                possible_top_x[total]=bottom_x, possible_top_y[total]=bottom_y, possible_bottom_x[total]=top_x, possible_bottom_y[total]=top_y;
-                total++;
+    int len = s.length();
+    for(int i = 1; i <= r; i++){
+        for(int j = 1; j <= c; j++){
+            for(int k = 0; k < 8; k++){
+                int nx = i, ny = j, tmp = 0;
+                while(1){
+                    if(tmp == len){
+                        ansx = nx - dir[k][0], ansy = ny - dir[k][1];                            
+                        cout << i << ' ' << j << '\n' << ansx << ' ' << ansy << '\n';
+                        return 0;
+                    }
+                    if(!in_range(nx, ny, r, c)) break;
+                    if(grid[nx][ny] == toupper(s[tmp]) || grid[nx][ny] == tolower(s[tmp])) tmp++;
+                    else break;
+                    nx += dir[k][0], ny += dir[k][1];
+                }
             }
         }
     }
-    // top left to bottom right, bottom right to top left
-    for(int i=0;i<r-s.length()+1;i++){
-        for(int j=0;j<c-s.length()+1;j++){
-            top_x=j, top_y=i, bottom_x=s.length()-1+j, bottom_y=s.length()-1+i;
-            if(ismatch(s, map, top_x, top_y, 1, -1)){
-                possible_top_x[total]=top_x, possible_top_y[total]=top_y, possible_bottom_x[total]=bottom_x, possible_bottom_y[total]=bottom_y;
-                total++;
-            }
-            if(ismatch(s, map, bottom_x, bottom_y, -1, 1)){
-                possible_top_x[total]=bottom_x, possible_top_y[total]=bottom_y, possible_bottom_x[total]=top_x, possible_bottom_y[total]=top_y;
-                total++;
-            }
-        }
-    }
-    // top right to bottom left, bottom left to top right
-    for(int i=0;i<r-s.length()+1;i++){
-        for(int j=0;j<c-s.length()+1;j++){
-            top_x=s.length()-1+j, top_y=s.length()-1+i, bottom_x=j, bottom_y=i;
-            if(ismatch(s, map, top_x, top_y, -1, -1)){
-                possible_top_x[total]=top_x, possible_top_y[total]=top_y, possible_bottom_x[total]=bottom_x, possible_bottom_y[total]=bottom_y;
-                total++;
-            }
-            if(ismatch(s, map, bottom_x, bottom_y, 1, 1)){
-                possible_top_x[total]=bottom_x, possible_top_y[total]=bottom_y, possible_bottom_x[total]=top_x, possible_bottom_y[total]=top_y;
-                total++;
-            }
-        }
-    }
-    
-    cout << "NO";
+    cout << "NO\n";
     return 0;
-
 }
-
-/*
-333 
-333 
-333
-//1
-    int checking=possible_top_y[0];same=1;min=INT_MAX;min_i=0;
-    for(int i=0;i<total;i++){
-        if(possible_top_y[i]<min){
-            min=possible_top_y[i];
-            min_i=i;
-        }
-        if(possible_top_y[i]==checking){
-            same++;
-        }
-    }
-    //2
-    if(same==total){
-        int checking=possible_top_x[0];same=1;min=INT_MAX;min_i=0;
-        for(int i=0;i<total;i++){
-            if(possible_top_x[i]<min){
-                min=possible_top_x[i];
-                min_i=i;
-            }
-            if(possible_top_x[i]==checking){
-                same++;
-            }
-        }
-    }
-    else{
-        printf("%d %d\n%d %d", possible_top_y[min_i]+1, possible_top_x[min_i]+1, possible_bottom_y[min_i]+1, possible_bottom_x[min_i]+1);
-        return 0;
-    }
-    //3
-    if(same==total){
-        int checking=possible_bottom_y[0];same=1;min=INT_MAX;min_i=0;
-        for(int i=0;i<total;i++){
-            if(possible_bottom_y[i]<min){
-                min=possible_bottom_x[i];
-                min_i=i;
-            }
-            if(possible_bottom_y[i]==checking){
-                same++;
-            }
-        }
-    }
-    else{
-        printf("%d %d\n%d %d", possible_top_y[min_i]+1, possible_top_x[min_i]+1, possible_bottom_y[min_i]+1, possible_bottom_x[min_i]+1);
-        return 0;
-    }
-    if(same==total){
-        int checking=possible_bottom_x[0];same=1;min=INT_MAX;min_i=0;
-        for(int i=0;i<total;i++){
-            if(possible_bottom_x[i]<min){
-                min=possible_top_x[i];
-                min_i=i;
-            }
-            if(possible_bottom_x[i]==checking){
-                same++;
-            }
-        }
-    }
-    else{
-        printf("%d %d\n%d %d", possible_top_y[min_i]+1, possible_top_x[min_i]+1, possible_bottom_y[min_i]+1, possible_bottom_x[min_i]+1);
-        return 0;
-    }
-    if(same!=total){
-        printf("%d %d\n%d %d", possible_top_y[min_i]+1, possible_top_x[min_i]+1, possible_bottom_y[min_i]+1, possible_bottom_x[min_i]+1);
-        return 0;
-    }
-    else{
-        printf("%d %d\n%d %d", possible_top_y[0]+1, possible_top_x[0]+1, possible_bottom_y[0]+1, possible_bottom_x[0]+1);
-        return 0;
-    }
-
-
-*/
